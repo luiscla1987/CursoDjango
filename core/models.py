83522b23e807eee896068bd0fd32ef9001bfefec
luiscla1987 <luiscla1987@gmail.com>
2022-05-06 21:10:57 -0300

    (livros): adiciona livros ao projeto

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from tabnanny import verbose
 from django.db import models
+from numpy import quantile
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
@@ -21,3 +22,14 @@ class Autor(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+
+    def __str__(self):
+        return "%s (%s)" %(self.titulo, self.editora)
