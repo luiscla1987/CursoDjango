e64d9a86482e92f53fce5088b42167c2a19e2060
luiscla1987 <luiscla1987@gmail.com>
2022-05-06 21:39:22 -0300

    (compra): adiciona compra ao projeto.

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
-from tabnanny import verbose
 from django.db import models
-from numpy import quantile
+from django.contrib.auth.models import User
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
@@ -30,6 +29,24 @@ class Livro(models.Model):
     preco = models.FloatField()
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+    autores = models.ManyToManyField(Autor, related_name="livros")
 
     def __str__(self):
         return "%s (%s)" %(self.titulo, self.editora)
+
+
+class Compra(models.Model):
+
+    class StatusCompra(models.IntegerChoices):
+        CARRINHO = 1, 'Carrinho'
+        REALIZADO = 2, 'Realizado'
+        PAGO = 3, 'Pago'
+        ENTREGUE = 4, 'Entregue'
+
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
+    status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+
+class ItensCompra(models.Model):
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
+    livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
+    quantidade = models.IntegerField()
