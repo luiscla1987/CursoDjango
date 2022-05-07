09ec56f10324f7d4c365acad9dd4f995e8b0d02e
luiscla1987 <luiscla1987@gmail.com>
2022-05-07 20:32:36 -0300

    feat(editora): Adiciona ModelViewSet Editora

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,19 +1,3 @@
-"""livraria URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,14 +6,15 @@ from rest_framework import routers
 from core import views
 
 router = routers.DefaultRouter()
-router.register(r'categorias-viewset', views.CategoriaViewSet)
+router.register(r'categorias', views.CategoriaViewSet)
+router.register(r'editoras', views.EditoraViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('teste/', views.teste),
     path('pag2/', views.teste2),
-    path('categorias/',views.CategoriaView.as_view()),
-    path('categorias/<int:id>/',views.CategoriaView.as_view()),
+    path('categorias-class/',views.CategoriaView.as_view()),
+    path('categorias-class/<int:id>/',views.CategoriaView.as_view()),
     path('categorias-apiview/',views.CategoriasList.as_view()),
     path('categorias-apiview/<int:id>/',views.CategoriaDetail.as_view()),
     path('categorias-generic/', views.CategoriasListGeneric.as_view()),
