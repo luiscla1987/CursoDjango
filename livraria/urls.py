f67e5b646850f7e503f8bb2e539e73a90d4c20c0
luiscla1987 <luiscla1987@gmail.com>
2022-05-07 18:55:54 -0300

    melhoria(dir): Ajusta diretórios.

--- livraria/urls.py
+++ livraria/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
 from core import views
+
+router = routers.DefaultRouter()
+router.register(r'categorias-viewset', views.CategoriaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +34,5 @@ urlpatterns = [
     path('categorias-apiview/<int:id>/',views.CategoriaDetail.as_view()),
     path('categorias-generic/', views.CategoriasListGeneric.as_view()),
     path('categorias-generic/<int:id>/',views.CategoriaDetailGeneric.as_view()),
+    path('', include(router.urls))
 ]
